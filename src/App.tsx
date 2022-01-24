bad1b918ec4419c4e6a37f0e777d4ad466750546
tagawa shouta <sho.webfe@gmail.com>
2022-01-24 18:49:15 +0900

    リファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,28 @@
-import { useState, useCallback, useRef, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import Provider from '@/providers';
-import { Box, Textarea, Container, HStack, Select, Button, Text, IconButton } from '@chakra-ui/react';
-import { Header } from '@/components';
-import { CopyIcon, CloseIcon } from '@chakra-ui/icons';
+import { Box, Container, Flex, Select, Button, Text } from '@chakra-ui/react';
+import { Header, Label, EditMessage } from '@/components';
+import { CopyIcon } from '@chakra-ui/icons';
+import emojis from '@/assets/emojis.json';
 
 function App() {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>('b');
   const [withEmojiValue, setWithEmojiValue] = useState<string>('');
   const [emoji, setEmoji] = useState<string>('bug');
   const [isCopied, setIsCopied] = useState<boolean>(false);
 
-  const options = [
-    {
-      prefix: 'バグ修正',
-      emoji: 'bug'
-    },
-    {
-      prefix: '機能改善',
-      emoji: '+1',
-    },
-    {
-      prefix: '部分的な機能改善',
-      emoji: 'sparkles'
-    },
-    {
-      prefix: '大きな機能追加',
-      emoji: 'tada'
-    },
-    {
-      prefix: 'パフォーマンス改善',
-      emoji: 'rocket'
-    },
-    {
-      prefix: 'セキュリティ関連の改善',
-      emoji: 'cop'
-    },
-    {
-      prefix: 'Lintエラーの修正やコードスタイルの修正',
-      emoji: 'shirt'
-    },
-    {
-      prefix: 'リファクタリング',
-      emoji: 'recycle'
-    },
-    {
-      prefix: 'ファイル、コード一部を削除',
-      emoji: 'shower'
-    },
-    {
-      prefix: 'ドキュメントの修正',
-      emoji: 'notebook'
-    },
-    {
-      prefix: 'テストやCIの修正・改善',
-      emoji: 'green_heart'
-    },
-    {
-      prefix: '依存パッケージなどのアップデート',
-      emoji: 'up',
-    }
-  ];
-
-  const onChangeInputValue = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const hanldeInput = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value);
     setIsCopied(false);
   }, []);
+
+  const handleDelete = () => setValue('');
 
   const copyTextToClipboard = () => {
     navigator.clipboard.writeText(withEmojiValue).then(() => {
       setIsCopied(true);
     })
   }
-
-  const deleteValue = () => setValue('');
 
   useEffect(() => {
     setWithEmojiValue(`:${emoji}: ${value}`);
@@ -84,34 +34,27 @@ function App() {
         <Header />
         <Container py={5} maxW="container.lg">
           <Box mb={5} pb={5} borderBottom='1px' borderColor='gray.200'>
-            <HStack spacing="24px" mb={5}>
-              <Text width="20%" flexShrink={0}>Prefix</Text>
+            <Flex mb={5} display={{ base: "display", md: "flex" }}>
+              <Label>Prefix</Label>
               <Select
-                width="70%"
+                width={{ base: "100%", md: "70%" }}
                 fontSize="14px"
                 onChange={e => setEmoji(e.target.value)}
                 value={emoji}
               >
-                {options.map(option => (
+                {emojis.map(option => (
                   <option key={option.emoji} value={option.emoji}>{option.prefix}</option>
                 ))}
               </Select>
-            </HStack>
-            <HStack spacing="24px">
-              <Text width="20%" flexShrink={0}>コミットメッセージ</Text>
-              <Textarea
-                resize="none"
-                width="70%"
-                fontSize="14px"
+            </Flex>
+            <Flex display={{ base: "display", md: "flex" }}>
+              <Label>コミットメッセージ</Label>
+              <EditMessage
                 value={value}
-                onChange={e => onChangeInputValue(e)}
+                onChange={e => hanldeInput(e)}
+                onDelete={handleDelete}
               />
-              <IconButton
-                aria-label="delete"
-                icon={<CloseIcon />}
-                onClick={deleteValue}
-              />
-            </HStack>
+            </Flex>
           </Box>
           <Box textAlign="center">
             <Button
